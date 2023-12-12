fafb0d72f1a289f49822d9d6535bfdb49d8d4718
weismark <marko.weissmann@gmail.com>
2023-12-12 02:46:04 -0500

    The latest changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,14 +5,15 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
 
     # path for about view
-
+    path(route='about', view=views.about, name="about"),
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
